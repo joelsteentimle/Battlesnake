89ff90f26d0afc7a4a097e0e70f313f055149590
Stefan Andersson <stefan.andersson@acorntechnology.se>
2017-03-27 11:27:18 +0200

    #Added WCF poc

---
+++ WcfServer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.ServiceModel.Web;
+
+namespace WcfServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new WebServiceHost(typeof(SnakeService), new Uri("http://0.0.0.0:80/"));
+            var ep = host.AddServiceEndpoint(typeof(ISnakeService), new WebHttpBinding(), "");            
+
+            host.Open();
+            Console.WriteLine("Service is running");
+            Console.WriteLine("Press enter to quit...");
+            Console.ReadLine();
+            host.Close();
+        }
+    }
+}
